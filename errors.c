a3cd8fd08ee3403d1c362d12aec19b19dda7a3ca
Sincerekiko <136067096+Sincerekiko@users.noreply.github.com>
2023-11-07 15:05:06 +0300

    Update errors.c

--- errors.c
+++ errors.c
@@ -62,22 +62,6 @@ i = 0;
 if (c != BUF_FLUSH)
 buf[i++] = c;
 return (1);
-}
-
-/**
- * _putsfd - prints an input string
- * @str: the string to be printed
- * @fd: the filedescriptor to write to
- *
- * Return: the number of chars put
- */
-int _putsfd(char *str, int fd)
-{
-int i = 0;
-
-if (!str)
-return (0);
-while (*str)
 {
 i += _putfd(*str++, fd);
 }
